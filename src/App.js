df489271471858561bd6480167137f6847511ac4
KimHoSoo217 <huhu0524@naver.com>
2025-05-16 15:29:19 +0900

    last

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import Update from "./pages/Update"
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={ProcessingInstruction.env.PUBLIC_URL}>
       <nav>
         <h1>Supa Smoothies</h1>
         <Link to="/">Home</Link>
         <Link to="/create">Create New Smoothie</Link>
+        <a href="http://www.daum.net">다음 사이트</a>
+        <a href="/smoothies">홈</a>
       </nav>
       <Routes>
         <Route path="/" element={<Home/>}/>
